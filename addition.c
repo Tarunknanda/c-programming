63a091341340021dbcf10547907000b180cb24ff
tarun Kumar Nanda <nandatarun2@gmail.com>
2019-02-16 18:59:07 +0530

    c programming

---
+++ addition.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+clrscr();
+int x,y,z;
+printf("enter the first number");
+scanf("%d",&x);
+printf("enter second number");
+scanf("%d",&y);
+z=x+y;
+printf("the addition is:%d",z);
+getch();
+}
